687fbb90c53f10cfcba7f84b7b7a12031a156d27
jmayta1984 <jmayta@pucp.edu.pe>
2023-06-04 10:28:44 -0500

    Add networking classes

--- app/src/main/java/pe/edu/cibertec/restaurantcompose/data/remote/RestaurantInterface.kt
+++ app/src/main/java/pe/edu/cibertec/restaurantcompose/data/remote/RestaurantInterface.kt
@@ -1,4 +1,11 @@
 package pe.edu.cibertec.restaurantcompose.data.remote
 
+import pe.edu.cibertec.restaurantcompose.data.model.Restaurant
+import retrofit2.Call
+import retrofit2.http.GET
+
 interface RestaurantInterface {
+    @GET("restaurants")
+    fun getRestaurants(): Call<List<Restaurant>>
+
 }
